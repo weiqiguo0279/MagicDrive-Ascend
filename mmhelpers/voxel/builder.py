cc07f5cf8ef1de4e5409974ccf68c1d7c4c14288
weiqiguo0279 <tadzioguo@gmail.com>
2025-12-30 15:16:59 +0800

    Add files via upload

---
+++ mmhelpers/voxel/builder.py
@@ -0,0 +1,13 @@
+from mmcv.runner import obj_from_dict
+
+from . import voxel_generator
+
+
+def build_voxel_generator(cfg, **kwargs):
+    """Builder of voxel generator."""
+    if isinstance(cfg, voxel_generator.VoxelGenerator):
+        return cfg
+    elif isinstance(cfg, dict):
+        return obj_from_dict(cfg, voxel_generator, default_args=kwargs)
+    else:
+        raise TypeError("Invalid type {} for building a sampler".format(type(cfg)))
